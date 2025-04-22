ce1e06eab4f7a205fe234ffd7bd85a9dbc40d785
Risto Rytilahti <Risto.rytilahti@edu.savonia.fi>
2025-04-22 20:09:58 +0300

    Generated project

--- src/main/java/com/example/application/views/mainview/MainView.java
+++ src/main/java/com/example/application/views/mainview/MainView.java
@@ -1,8 +1,10 @@
 package com.example.application.views.mainview;
 
+
 import com.example.application.data.User;
 import com.example.application.security.AuthenticatedUser;
 import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -14,20 +16,30 @@ import com.vaadin.flow.component.html.Image;
 @PageTitle("Etusivu | Leffakerho")
 @Route(value = "", layout = MainLayout.class)
 @RolesAllowed({"ROLE_USER", "ROLE_ADMIN"})
+@CssImport("./themes/leffakerho/views/main-view.css")
 public class MainView extends VerticalLayout {
 
     public MainView(AuthenticatedUser authenticatedUser) {
+        setClassName("main-view");
         setSpacing(true);
         setPadding(true);
+
+
+        // Käytetään Lumo Utility -luokkia
+        addClassNames("lumo-padding-l", "lumo-margin-m");
 
         User user = authenticatedUser.get().orElse(null);
         String username = user != null ? user.getName() : "Käyttäjä";
 
         H1 header = new H1("Tervetuloa, " + username + "!");
-        Paragraph description = new Paragraph("Voit selata elokuvia, lukea ja kirjoittaa arvosteluja sekä lisätä suosikkeja omaan listaan.");
+        header.addClassNames("main-header", "lumo-text-contrast");
 
         Image logo = new Image("/images/logo.png", "Leffakerho logo");
         logo.setWidth("400px");
+        logo.addClassNames("logo", "lumo-border-radius");
+
+        Paragraph description = new Paragraph("Voit selata elokuvia, lukea ja kirjoittaa arvosteluja sekä lisätä suosikkeja omaan listaan.");
+        description.addClassNames("lumo-font-size-m");
 
         add(header, logo, description);
     }
